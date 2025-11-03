44bbfab6c5c8b104790dbaae6f72f094a2308f12
Paul2000-2000 <alexapaul2000@yahoo.com>
2025-11-03 17:21:22 +0200

    big upgrades for navbar

---
+++ client/hotel-booking/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+
+import { useState } from 'react';
+
+import { CgProfile } from "react-icons/cg";
+import { IoMenu } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
+
+import styles from './Navbar.module.scss'
+
+ const Navbar = () => {
+
+    const [active , setActive] = useState(true);
+
+
+  return (
+    <div className={styles.class}>
+
+        {
+
+            active ?
+
+            <div className={styles.content}>
+                <h1 className={styles.logo}>trai Hotels</h1>
+                <ul className={styles.menu}>
+                    <li className={styles.priority}>Home</li>
+                    <li className={styles.item}>Hotels for sale</li>
+                    <li className={styles.item}>News</li>
+                    <li className={styles.item}>Contacts</li>
+                    <div className={styles.sign} >
+                        <p>Sign In</p>
+                        <CgProfile className={styles.icon}/>
+                    </div>
+                
+                </ul>
+                <IoMenu className={styles.icon} onClick={ () => setActive(!active)}/>
+            </div>
+
+            :
+
+            <div className={styles.contentResp}>
+
+                    <IoMdClose className={styles.close} onClick={ () => setActive(!active)}/>
+                    <div className={styles.priority}>Home</div>
+                    <div className={styles.item}>Hotels for sale</div>
+                    <div className={styles.item}>News</div>
+                    <div className={styles.item}>Contacts</div>
+                    <div className={styles.sign} >
+                        <p>Sign In</p>
+                        <CgProfile className={styles.icon}/>
+                    </div>
+
+            </div>
+
+        }
+
+
+        
+    </div>
+  )
+}
+
+export default Navbar;
