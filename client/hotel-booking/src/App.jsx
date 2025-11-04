29f55aa9b8390b7266fd290e62e1528168872f10
Paul2000-2000 <alexapaul2000@yahoo.com>
2025-11-04 11:02:48 +0200

    almost done with register/login design

--- client/hotel-booking/src/App.jsx
+++ client/hotel-booking/src/App.jsx
@@ -1,12 +1,21 @@
 import HomePage from "./pages/HomePage/HomePage"
+import LoRePage from "./pages/LoRePage/LoRePage"
+
+import {BrowserRouter , Route, Routes} from 'react-router-dom'
 
 function App() {
   
 
   return (
-    <>
-      <HomePage/>
-    </>
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/lorepage" element={<LoRePage/>} />
+      </Routes>
+    </BrowserRouter>
+     
+    
   )
 }
 
