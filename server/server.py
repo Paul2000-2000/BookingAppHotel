c343e01624579ba03c71bfbfc54eaad7cdd6f844
Paul2000-2000 <alexapaul2000@yahoo.com>
2025-11-05 11:56:41 +0200

    making relation between db and server, add user almost done

--- server/server.py
+++ server/server.py
@@ -1,12 +1,51 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify , request
 from flask_cors import CORS
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)  
 
-@app.route("/members")
-def member():
-    return jsonify({"members": ["Member1", "Member2"]})
+uri = os.getenv("MONGO_URI")
+
+
+client = MongoClient(uri)
+
+
+
+
+
+
+@app.route("/addUser" ,  methods=["POST"])
+def addUser():
+ 
+    data = request.get_json()
+
+    email = data.get("emailRegister")
+    password = data.get("passwordRegister")
+    name = data.get("nameRegister")
+
+    db = client["hotels_and_users"] ## selectam baza de date
+    users_collection = db["users"] ## selectez colectia din baza de date
+
+    new_user = {
+        "email": email,
+        "password": password,  # eventual hash-uieste parola!
+        "name": name
+    }
+
+    users_collection.insert_one(new_user)
+    
+    return jsonify({"message": "User added successfully"}), 200
+
+
+
+
+
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
