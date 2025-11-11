ef35aca89ec3c6c76d2ff2be4ce72a98fabc6b44
Paul2000-2000 <alexapaul2000@yahoo.com>
2025-11-11 10:05:28 +0200

    almost finish new Hotels view design

--- client/hotel-booking/src/pages/HomePage/HomePage.jsx
+++ client/hotel-booking/src/pages/HomePage/HomePage.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 
-import styles from './HomePage.module.scss';
+// import styles from './HomePage.module.scss';
 
 import Navbar from '../../components/Navbar/Navbar';
 import Hero from '../../views/Hero/Hero';
+import NewHotels from '../../views/NewHotels/NewHotels';
 
  const HomePage = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
-      
+      <NewHotels/>
     </div>
   )
 }
